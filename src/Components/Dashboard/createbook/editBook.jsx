a99922b6cb5029ba21121c3741c3f52c4f1edd3e
Shrirama Kamath <shriramakamath2002@gmail.com>
2024-09-24 12:38:14 +0530

    fixed a bug in edit book details

--- src/Components/Dashboard/createbook/editBook.jsx
+++ src/Components/Dashboard/createbook/editBook.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from '../../../axiosConfig'; // Your axios configuration
-import { TextField, Button, MenuItem,FormControl,InputLabel } from '@mui/material';
+import { TextField, Button, MenuItem,FormControl,InputLabel,Grid,Typography,Alert } from '@mui/material';
 
 const EditBook = () => {
   const { bookId } = useParams(); // Assuming you're passing the bookId as a route parameter
@@ -13,14 +13,18 @@ const EditBook = () => {
     coverImage: null
   });
 
+const [message, setMessage] = React.useState(); // State for success or error message
+
   // Fetch the current book data
   useEffect(() => {
     axios.get(`/books/${bookId}`)
       .then(response => {
         setBook(response.data.book);
+        
       })
       .catch(error => {
-        console.error('Error fetching book data:', error);
+        console.error('Error updating book data:', error);
+        
       });
   }, [bookId]);
 
@@ -59,16 +63,29 @@ const EditBook = () => {
       }
     })
       .then(response => {
+        setMessage({type: 'success', text: 'Book details updated successfully!' });
         alert('Book updated successfully!');
         navigate(`/dashboard`); // Navigate to the dashboard or any relevant page
       })
       .catch(error => {
         console.error('Error updating the book:', error);
-        alert('There was an error updating the book.');
+        setMessage({ type: 'error', text: 'Please try again later.' });
+        //alert('There was an error updating the book.');
       });
   };
 
   return (
+    <Grid container justifyContent="center">
+      <Grid item xs={12} sm={8} md={6}>
+        <Typography variant="h5" component="h1">
+          Update Book Details
+        </Typography>
+        {/* Display success or error message */}
+        {message && (
+            <Alert severity={message.type} sx={{ width: '100%', mt: 2 }}>
+              {message.text}
+            </Alert>
+          )}
     <form onSubmit={handleEditClick} encType="multipart/form-data">
       <TextField
         fullWidth
@@ -108,7 +125,7 @@ const EditBook = () => {
               name="coverImage"
               accept="image/*"
               onChange={handleFileChange}
-              required
+              required 
               style={{ marginTop: '16px' }}
             />
           </FormControl>
@@ -122,6 +139,8 @@ const EditBook = () => {
         Save Changes
       </Button>
     </form>
+  </Grid>
+</Grid>
   );
 };
 
